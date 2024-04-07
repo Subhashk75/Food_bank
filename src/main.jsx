b55313891c000669390fe170a3104fde4a55389c
RajeshKumawat0101 <142051345+RajeshKumawat0101@users.noreply.github.com>
2024-04-07 15:22:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import About from './components/About'
+import Contact from './components/Contact'
+import Body from './components/Body'
+import DonateForm from './components/DonateForm'
+import Profile from './components/Profile'
+import Error from './components/Error'
 import './index.css'
-
+ // call createBrowserRouter for routing different pages
+const appRouter = createBrowserRouter([
+  {
+    path: "/", // show path for routing
+    element: <App/>, // show component for particular path
+    errorElement: <Error />, // show error component for path is different
+    children: [
+      // show children component for routing
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path:"/donatefood",
+        element:<DonateForm/>
+      }
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={appRouter} />
   </React.StrictMode>,
 )
